6fcc280bb95d89d657ed140726dcefdead647bce
ArtemNizelnyk <arn@sitecore.net>
2017-04-12 19:19:15 +0300

    Initial Commit

---
+++ src/Sitecore.Support.157139/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.157139")]
+[assembly: AssemblyProduct("Sitecore.Support.157139")]
+[assembly: ComVisible(false)]
